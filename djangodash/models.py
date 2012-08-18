409fb9d46b890563549d83147e3e95c315dd766b
Noam Sutskever <noamsu@gmail.com>
2012-08-18 10:36:54 -0400

    Adding Comment model (with migration).

--- djangodash/models.py
+++ djangodash/models.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class Comment(models.Model):
+	"""
+	Represents a comment.
+	"""
+
+	author = models.ForeignKey(User)
+	content = models.TextField("Comment body")
+	parent = models.ForeignKey("self", null=True, blank=True)
+	thread = models.ForeignKey("Thread")
+	votes = models.PositiveIntegerField(default=0)
+
+	
+
+
+
 class Thread(models.Model):
 	"""
 	Represents a Thread.
